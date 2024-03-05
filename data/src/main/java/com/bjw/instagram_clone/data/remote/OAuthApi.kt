adfbdd15324d74ff0cc93bfbd89e6d4decc198c1
dev-baik <wlsdn9297@naver.com>
2024-03-05 18:29:56 +0900

    feat: api post로 수정

--- data/src/main/java/com/bjw/instagram_clone/data/remote/OAuthApi.kt
+++ data/src/main/java/com/bjw/instagram_clone/data/remote/OAuthApi.kt
@@ -2,9 +2,13 @@ package com.bjw.instagram_clone.data.remote
 
 import com.bjw.instagram_clone.data.model.MockApiResponse
 import retrofit2.Response
+import retrofit2.http.Field
 import retrofit2.http.POST
 
 interface OAuthApi {
     @POST("auth/login")
-    suspend fun getMockApi(): Response<MockApiResponse>
+    suspend fun getMockApi(
+        @Field("Type") type: String,
+        @Field("token") token: String,
+    ): Response<MockApiResponse>
 }
