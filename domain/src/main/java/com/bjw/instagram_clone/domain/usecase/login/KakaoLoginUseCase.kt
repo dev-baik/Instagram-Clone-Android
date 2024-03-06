c53af6a5dade2de336aaccf9735121fda2f41e9b
dev-baik <wlsdn9297@naver.com>
2024-03-06 15:51:17 +0900

    feat: UseCase, BaseRepository 맞춰서 수정

--- domain/src/main/java/com/bjw/instagram_clone/domain/usecase/login/KakaoLoginUseCase.kt
+++ domain/src/main/java/com/bjw/instagram_clone/domain/usecase/login/KakaoLoginUseCase.kt
@@ -2,10 +2,13 @@ package com.bjw.instagram_clone.domain.usecase.login
 
 import com.bjw.instagram_clone.domain.model.MockApiItem
 import com.bjw.instagram_clone.domain.repository.MockRepository
+import com.bjw.instagram_clone.domain.utils.RemoteErrorEmitter
 import javax.inject.Inject
 
 class KakaoLoginUseCase @Inject constructor(
     private val mockRepository: MockRepository
 ) {
-    suspend fun execute(token: String): MockApiItem? = mockRepository.getKakaoMock(token = token)
+    suspend fun execute(remoteErrorEmitter: RemoteErrorEmitter, token: String): MockApiItem? {
+        return mockRepository.getKakaoMock(remoteErrorEmitter, token = token)
+    }
 }
