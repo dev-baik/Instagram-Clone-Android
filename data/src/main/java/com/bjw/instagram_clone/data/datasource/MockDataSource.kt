3e12645887a923cf37c9c23e5bbbba8c79d0ba49
dev-baik <wlsdn9297@naver.com>
2024-03-06 15:49:00 +0900

    feat: DataSource, BaseRepository 사용해서 내부 구현

--- data/src/main/java/com/bjw/instagram_clone/data/datasource/MockDataSource.kt
+++ data/src/main/java/com/bjw/instagram_clone/data/datasource/MockDataSource.kt
@@ -1,7 +1,8 @@
 package com.bjw.instagram_clone.data.datasource
 
 import com.bjw.instagram_clone.data.model.MockApiResponse
+import com.bjw.instagram_clone.domain.utils.RemoteErrorEmitter
 
 interface MockDataSource {
-    suspend fun getKakaoMock(token: String): MockApiResponse?
+    suspend fun getKakaoMock(remoteErrorEmitter: RemoteErrorEmitter, token: String): MockApiResponse?
 }
--- data/src/main/java/com/bjw/instagram_clone/data/datasource/impl/MockDataSourceImpl.kt
+++ data/src/main/java/com/bjw/instagram_clone/data/datasource/impl/MockDataSourceImpl.kt
@@ -3,12 +3,16 @@ package com.bjw.instagram_clone.data.datasource.impl
 import com.bjw.instagram_clone.data.datasource.MockDataSource
 import com.bjw.instagram_clone.data.model.MockApiResponse
 import com.bjw.instagram_clone.data.remote.OAuthApi
+import com.bjw.instagram_clone.data.utils.BaseRepository
+import com.bjw.instagram_clone.domain.utils.RemoteErrorEmitter
 import javax.inject.Inject
 
 class MockDataSourceImpl @Inject constructor(
     private val oAuthApi: OAuthApi
-) : MockDataSource {
-    override suspend fun getKakaoMock(token: String): MockApiResponse? {
-        TODO("Not yet implemented")
+) : BaseRepository(), MockDataSource {
+    override suspend fun getKakaoMock(remoteErrorEmitter: RemoteErrorEmitter, token: String): MockApiResponse? {
+        return safeApiCall(remoteErrorEmitter) {
+            oAuthApi.getMockApi("kakao", token).body()
+        }
     }
 }
