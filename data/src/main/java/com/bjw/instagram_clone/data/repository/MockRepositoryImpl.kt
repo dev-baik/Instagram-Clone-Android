8d431a67a6f0d74f5e9cfca256d7ac46efb15ced
dev-baik <wlsdn9297@naver.com>
2024-03-06 15:49:31 +0900

    feat: Repository, BaseRepository 맞춰서 수정

--- data/src/main/java/com/bjw/instagram_clone/data/repository/MockRepositoryImpl.kt
+++ data/src/main/java/com/bjw/instagram_clone/data/repository/MockRepositoryImpl.kt
@@ -4,12 +4,18 @@ import com.bjw.instagram_clone.data.datasource.MockDataSource
 import com.bjw.instagram_clone.data.mapper.MapperToDomain
 import com.bjw.instagram_clone.domain.model.MockApiItem
 import com.bjw.instagram_clone.domain.repository.MockRepository
+import com.bjw.instagram_clone.domain.utils.RemoteErrorEmitter
 import javax.inject.Inject
 
 class MockRepositoryImpl @Inject constructor(
     private val mockDataSource: MockDataSource
 ) : MockRepository {
-    override suspend fun getKakaoMock(token: String): MockApiItem? {
-        return MapperToDomain.mapperToMockApiItem(mockDataSource.getKakaoMock(token = token))
+    override suspend fun getKakaoMock(
+        remoteErrorEmitter: RemoteErrorEmitter,
+        token: String
+    ): MockApiItem? {
+        return MapperToDomain.mapperToMockApiItem(
+            mockDataSource.getKakaoMock(remoteErrorEmitter, token = token)
+        )
     }
 }
--- domain/src/main/java/com/bjw/instagram_clone/domain/repository/MockRepository.kt
+++ domain/src/main/java/com/bjw/instagram_clone/domain/repository/MockRepository.kt
@@ -1,7 +1,8 @@
 package com.bjw.instagram_clone.domain.repository
 
 import com.bjw.instagram_clone.domain.model.MockApiItem
+import com.bjw.instagram_clone.domain.utils.RemoteErrorEmitter
 
 interface MockRepository {
-    suspend fun getKakaoMock(token: String): MockApiItem?
+    suspend fun getKakaoMock(remoteErrorEmitter: RemoteErrorEmitter, token: String): MockApiItem?
 }
